cc772dca65ec807f29f4b31ffb1cd1d8064495fc
peshoFT <vikikoy@smg.bg>
2021-09-15 08:00:30 +0300

    sample file download

---
+++ app/urls.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
+
+from . import views
+
+urlpatterns = [
+    path("", views.index, name="index"),
+    path("download/<path:path>", views.download, name="download"),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- app/views.py
+++ app/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.http import FileResponse
 
-# Create your views here.
+
+def index(request):
+    return render(request, "app/index.html", {})
+
+
+def download(request, path):
+    return FileResponse(open("media/stoichi.jpg", "rb"))
+
+
+# spotdl songurl --output media/
